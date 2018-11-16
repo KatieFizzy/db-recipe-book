4b348b4cce1717faef855b73de98c36d972da328
Katie Cleary <katiecleary@airmaster.lan>
2018-11-16 09:45:45 -0500

    created shopping list table function

--- db/migrations/20181116085836_shoppingList.js
+++ db/migrations/20181116085836_shoppingList.js
@@ -1,8 +1,34 @@
 
 exports.up = function(knex, Promise) {
-  
-};
+/* SHOPPING LIST create table function - Holds list of 
+ingredients per recipe added with associated qty, tied to 
+recipeIngredients_ID
+*/
+   return knex.schema.createTable('shoppingList', function(tbl) {
+    // make changes to the table using the tbl object passed as a parameter
+    
+    // primary key
+    tbl.increments(); 
+    
+    // other fields
+    tbl.string('ingredient_type', 255)
 
+    tbl.real('quantity')
+   
+    tbl.string('quantity_unit', 50)
+       .unique('quantity_unit');
+    
+    //foreign id
+    tbl
+    .integer('recipeIngredient_id')
+    .unsigned()
+    .references('id')
+    .inTable('recipeIngredients');
+    
+     });
+    };
 exports.down = function(knex, Promise) {
-  
+// undo the changes to the database (it's called rolling back changes)
+return knex.schema.dropTableIfExists('shoppingList');
 };
+            
