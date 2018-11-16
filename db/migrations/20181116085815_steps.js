cc08f3be0b66cf3c64a0ca4f1d4b3baaad7afcc5
Katie Cleary <katiecleary@airmaster.lan>
2018-11-16 09:18:48 -0500

    created steps table migration

---
+++ db/migrations/20181116085815_steps.js
@@ -0,0 +1,38 @@
+
+exports.up = function(knex, Promise) {
+/* STEPS create table function - holds list of all steps, 
+with a column to tie steps to a specific recipeIngredientsID, and a column
+for a optional step sequence number to be added. Ties recipeIngredientsID
+vs recipe ID so it has ability to refer to ingredients and quantity
+if needed in some frontend scenario*/
+    
+return knex.schema.createTable('steps', function(tbl) {
+    // make changes to the table using the tbl object passed as a parameter
+    
+    // primary key
+    tbl.increments(); 
+    
+    // other fields
+    tbl.string('step', 355)
+       .unique('step');
+
+    tbl.integer('sequence number')
+       .unique('sequence number');
+    
+    //foreign id
+    tbl
+    .integer('recipeIngredient_id')
+    .unsigned()
+    .references('id')
+    .inTable('recipeIngredients');
+    
+     });
+   }
+
+     exports.down = function(knex, Promise) {
+      // undo the changes to the database (it's called rolling back changes)
+      return knex.schema.dropTableIfExists('steps');
+    };
+    
+    
+  
